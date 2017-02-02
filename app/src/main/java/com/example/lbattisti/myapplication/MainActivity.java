bc3b09032d40212405b1e9e90bafb9cbfdd2bb24
Leandro Vilson Battisti <asengardeons@hotmail.com>
2017-02-02 20:49:49 -0200

    first commit

---
+++ app/src/main/java/com/example/lbattisti/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.lbattisti.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://almsaeedstudio.com/themes/AdminLTE/index2.html");
+    }
+}
